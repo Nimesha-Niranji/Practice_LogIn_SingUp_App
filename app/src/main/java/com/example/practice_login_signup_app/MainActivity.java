5f71bc2622813ef04e365ebedab896387e919f46
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-11-04 17:31:08 +0530

    Create an activity SignUpActivity

--- app/src/main/java/com/example/practice_login_signup_app/MainActivity.java
+++ app/src/main/java/com/example/practice_login_signup_app/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.practice_login_signup_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edUserName, edPassword;
+    Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edUserName = findViewById(R.id.editUserName);
+        edPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.btnlogin);
+        btnSignup = findViewById(R.id.btnsignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
